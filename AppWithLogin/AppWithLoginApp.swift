a603141566a4e7b80eafeac87adf4e4f306ff9fa
Vikash Anand <anandin02@gmail.com>
2024-12-01 21:05:50 +0530

    Initial Commit

---
+++ AppWithLogin/AppWithLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppWithLoginApp.swift
+//  AppWithLogin
+//
+//  Created by Vikash Anand on 01/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AppWithLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
